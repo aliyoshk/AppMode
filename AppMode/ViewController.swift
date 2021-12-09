49e48bd4de950c6a23fccaebd7a280ca49b7e007
stephen nathaniel <stephennathaniel89@gamil.com>
2021-12-09 12:17:10 +0100

    First_Commit

--- AppMode/ViewController.swift
+++ AppMode/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var lbl: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        self.imgView.image = UIImage(named: "tree")
     }
 
 
+    @IBAction func btnClick(_ sender: Any) {
+        self.btn.setTitle("Oasis", for: .normal)
+        self.lbl.text = "Click"
+    }
 }
 
